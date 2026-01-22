39112454d371ca1acf3c4ed48ede083569c5b32d
vijayendra-b <vijayendraswamy@gmail.com>
2026-01-22 16:40:02 +0530

    updated app.py

--- app/app.py
+++ app/app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "CI/CD Pipeline with Jenkins and Ansible "
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return f"CI/CD Pipeline with Jenkins and Ansible | Time: {timestamp}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
